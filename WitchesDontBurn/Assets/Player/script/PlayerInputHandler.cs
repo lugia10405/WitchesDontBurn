100388e366737c017770fd1871b6f59258497fea
yifyu1122 <yifyu1122@gmail.com>
2025-12-06 20:02:43 +0800

    WaterShootingMechanicUpdate

--- WitchesDontBurn/Assets/Player/script/PlayerInputHandler.cs
+++ WitchesDontBurn/Assets/Player/script/PlayerInputHandler.cs
@@ -12,28 +12,44 @@ public class PlayerInputHandler : MonoBehaviour
         flyActions = InputSystem.actions.FindAction("Move");
         broomActions = InputSystem.actions.FindAction("Interact");
         shootWater = InputSystem.actions.FindAction("Attack");
+    }
 
-
-        // enable actions and subscribe safely
+    private void OnEnable()
+    {
         if (flyActions != null)
-        {
             flyActions.Enable();
-        }
-
 
         if (broomActions != null)
         {
+            broomActions.performed -= Carry;  // 先移除舊訂閱
             broomActions.performed += Carry;
             broomActions.Enable();
         }
 
         if (shootWater != null)
         {
+            shootWater.performed -= ShootWater;  // 先移除舊訂閱
             shootWater.performed += ShootWater;
             shootWater.Enable();
         }
-        
+    }
 
+    private void OnDisable()
+    {
+        if (broomActions != null)
+        {
+            broomActions.performed -= Carry;
+            broomActions.Disable();
+        }
+
+        if (shootWater != null)
+        {
+            shootWater.performed -= ShootWater;
+            shootWater.Disable();
+        }
+
+        if (flyActions != null)
+            flyActions.Disable();
     }
 
     private void Carry(InputAction.CallbackContext context)
